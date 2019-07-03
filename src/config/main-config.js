97aeb0934cf41ce52f40eaaad1bdbe06ac31ff8c
Kerry Jackson <kerry.jackson108@gmail.com>
2019-07-03 13:51:20 -0400

    setup complete

---
+++ src/config/main-config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+
+module.exports = {
+	init(app, express) {
+	app.use(logger("dev"));
+	app.use(bodyParser);
+	}
+}
